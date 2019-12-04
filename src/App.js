04eff044700fc19452fa659b775490260240bb74
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-04 01:21:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./App.css";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
+
+import logo from "./assets/logobo.svg";
+
+function App() {
+  const [address, setAddress] = React.useState("");
+  const [coordinates, setCoordinates] = React.useState({
+    lat: null,
+    lng: null
+  });
+
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setCoordinates(latLng);
+    await setAddress(value);
+    return alert("Voce esta pesquisando promoções em " + address);
+  };
+
+  return (
+    <div className="container">
+      <div className="content">
+        <img src={logo} alt="BuscaOferta" />
+        <p>
+          Escolha sua <strong>cidade</strong> para que possamos pesquisar
+          ofertas
+        </p>
+
+        <form>
+          <PlacesAutocomplete
+            value={address}
+            onChange={setAddress}
+            onSelect={handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading
+            }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: "Digite o endereço da sua localização",
+                    className: "location-search-input"
+                  })}
+                />
+                <div>
+                  {loading ? <div>...loading</div> : null}
+                  {suggestions.map(suggestion => {
+                    const style = {
+                      backgroundColor: suggestion.active ? "#59125941" : "#fff"
+                    };
+
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          style,
+                          className: "inputSuggestion"
+                        })}
+                      >
+                        {suggestion.description}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+          {/* <button className="btn-search" onClick={handleSelect()}>
+            Pesquisar
+          </button> */}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
